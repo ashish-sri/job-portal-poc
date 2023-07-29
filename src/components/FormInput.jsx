b907ba42c691ce469e0c70aaa264a3262610d3f0
ashish-sri <atsashishsrivastava@gmail.com>
2023-07-29 15:11:40 +0530

    breaking div issue

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -21,7 +21,8 @@ function FormInput(props){
             <p className={
                 getTailwindClass(props.tailwind.TITLE)
             }>{props.title}
-                <span className={getTailwindClass(props.tailwind.TITLE.WARNING)}>{warningState}</span>
+                <span className={getTailwindClass(props.tailwind.TITLE.WARNING)} >{warningState}</span>
+                <span>&#160;</span>
             </p>
             <input type="text" placeholder={props.placeholder} 
                 className={
